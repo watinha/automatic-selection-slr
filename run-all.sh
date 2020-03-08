1778bc01d42d110cabbf103f81c3e527d93e3dff
Willian Watanabe <watinha@gmail.com>
2020-03-08 17:49:08 -0300

    little fixes for controling the output format

--- run-all.sh
+++ run-all.sh
@@ -1,8 +1,8 @@
-python3 main.py games > games-output.txt
-python3 main.py slr > slr-output.txt
-python3 main.py pair > pair-output.txt
-python3 main.py illiterate > illiterate-output.txt
-python3 main.py mdwe > mdwe-output.txt
-python3 main.py xbi > xbi-output.txt
-python3 main.py testing > testing-output.txt
-python3 main.py ontologies > ontologies-output.txt
+python3 main.py games > result/games-output.txt
+python3 main.py slr > result/slr-output.txt
+python3 main.py pair > result/pair-output.txt
+python3 main.py illiterate > result/illiterate-output.txt
+python3 main.py mdwe > result/mdwe-output.txt
+python3 main.py xbi > result/xbi-output.txt
+python3 main.py testing > result/testing-output.txt
+python3 main.py ontologies > result/ontologies-output.txt
