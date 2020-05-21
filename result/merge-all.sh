26df457a60011369bb02944b1572c45a1e157835
Willian Watanabe <watinha@gmail.com>
2020-05-21 19:11:13 -0300

    moving experiment results to a separate folder and updating merge python script

---
+++ result/merge-all.sh
@@ -0,0 +1,7 @@
+python merge.py precision
+python merge.py recall
+python merge.py fscore
+python merge.py exclusion
+python merge.py missed
+python merge.py exclusion-baseline
+python merge.py missed-baseline
--- result/merge.py
+++ result/merge.py
@@ -2,15 +2,27 @@ import pandas as pd
 
 files = ['games.csv', 'mdwe.csv', 'pair.csv', 'testing.csv',
          'illiterate.csv', 'ontologies.csv', 'slr.csv', 'xbi.csv']
-#columns = [0, 1, 2] # precision
-#columns = [0, 3, 4] # recall
-#columns = [0, 5, 6] # fscore
-#columns = [0, 7, 8] # correct exclusion
+
+argument = sys.argv[2]
+if argument == 'precision':
+    columns = [0, 1, 2] # precision
+if argument == 'recall':
+    columns = [0, 3, 4] # recall
+if argument == 'fscore':
+    columns = [0, 5, 6] # fscore
+
 #columns = [0, 9, 10] # threashold
-#columns = [0, 11, 12] # missed
 #columns = [0, 13, 14] # fscore with threashold
-#columns = [0, 15, 16] # exclusion baseline
-columns = [0, 17, 18] # missed baseline
+
+if argument == 'exclusion':
+    columns = [0, 7, 8] # correct exclusion
+if argument == 'missed':
+    columns = [0, 11, 12] # missed
+if argument == 'exclusion-baseline':
+    columns = [0, 15, 16] # exclusion baseline
+if argument == 'missed-baseline':
+    columns = [0, 17, 18] # missed baseline
+
 folders = [ 'k50', 'k100', 'k300', 'k1000', 'k3000' ]
 
 complete_table = pd.DataFrame()
@@ -32,6 +44,6 @@ for folder in folders:
     else:
         complete_table = complete_table.merge(table, left_on=['fold', 'group'], right_on=['fold', 'group'])
 
-f = open('all-missed-baseline.csv', 'w')
+f = open('all-%s.csv' % (argument), 'w')
 f.write(complete_table.to_csv())
 f.close()
